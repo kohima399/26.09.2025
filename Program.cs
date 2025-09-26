9b8fbdad1dce252ba2559db05aa44c86b928abdb
kohima399 <ikingagario@gmail.com>
2025-09-26 10:27:14 +0300

    Добавляем в класс "FootballTeam" переменные.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace ConsoleApp3
 {
     class FootballTeam
     {
+        public string Name { get; set; } //Название команд
+        public string Coach { get; set; } //Тренер команды
+        public int Points { get; set; } //Количество набранных очков
+        public int Place { get; set; } //Текущее место
 
     }
     internal class Program
