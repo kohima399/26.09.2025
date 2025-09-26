d2df1dbce0d47e52fb694a1e308ad260a3dec639
kohima399 <ikingagario@gmail.com>
2025-09-26 10:33:40 +0300

    Добавляем static void Show и вставляем перед объектом с названием = "Комета" новый объект

--- Program.cs
+++ Program.cs
@@ -60,6 +60,37 @@ namespace ConsoleApp3
                 Points = 18,
                 Place = 6
             });
+
+            Show(teams, "Начальное содержание");
+
+            // вставляем перед объектом с Названием = Комета новый объект
+            int cometIndex = teams.FindIndex(t => t.Name == "Комета");
+            if (cometIndex != -1)
+            {
+                teams.Insert(cometIndex, new FootballTeam()
+                {
+                    Name = "Звезда",
+                    Coach = "Тетерин",
+                    Points = 14,
+                    Place = 9
+                });
+            }
+            Show(teams, "Вставлена команда Звезда перед Кометой");
+
         }
+        static void Show(List<FootballTeam> col, string comment)
+        {
+            Console.Write("//////////////");
+            Console.Write(" " + comment + " ");
+            Console.WriteLine("//////////////");
+            // цикл по объектам коллекции
+            foreach (FootballTeam team in col)
+            {
+                Console.Write($"Индекс = {col.IndexOf(team)}; ");
+                Console.WriteLine($"Команда: {team.Name}, Тренер: {team.Coach}, Очки: {team.Points}, Место: {team.Place}");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
